0bff6f2c140a8e4a3ef2dccf428532b4f9f85570
ventodeco <ventodecoboagoes@gmail.com>
2021-05-12 00:57:40 +0700

    basic infrastructure

---
+++ README.md
@@ -0,0 +1,4 @@
+#Crowdfunding Project
+
+Tech stack that i use in this project are Golang, Vue JS, Nuxt JS
+
---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bwastartup/user"
+	"log"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	dsn := "root:root@tcp(127.0.0.1:3306)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	userRepository := user.NewRepository(db)
+	userService := user.NewService(userRepository)
+
+	userInput := user.RegisterUserInput{}
+	userInput.Name = "Tes simpan dari service"
+	userInput.Email = "vento@mail.com"
+	userInput.Occupation = "anak band"
+	userInput.Password = "password"
+
+	userService.RegisterUser(userInput)
+}
